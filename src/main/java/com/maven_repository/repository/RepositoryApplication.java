973e622cdfd1369263f5a254499f0314d6bbb470
murshid8095 <alamm.ait.070@gmail.com>
2025-11-05 10:50:23 +0530

    sencond Message

--- src/main/java/com/maven_repository/repository/RepositoryApplication.java
+++ src/main/java/com/maven_repository/repository/RepositoryApplication.java
@@ -8,6 +8,7 @@ public class RepositoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RepositoryApplication.class, args);
+		System.out.println("Hello world..!");
 	}
 
 }
